1041b5ad7729947f38b221d82aa1c38683f94fda
Abrahan N <35606009+zenith110@users.noreply.github.com>
2021-08-17 15:40:05 -0400

    Added prof kang

--- src/cogs/Roles.py
+++ src/cogs/Roles.py
@@ -148,6 +148,7 @@ class Roles(commands.Cog):
                         "jones",
                         "reyes",
                         "dhalla",
+                        "kang"
                     ]
                 )
 
